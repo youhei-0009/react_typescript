abfaaa91db97ec8b5a0da87a1836011a7557b65a
youhei-0009 <57719055+youhei-0009@users.noreply.github.com>
2024-06-11 19:50:45 +0900

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import './App.css';
+
+function App() {
+  const onClickFetchData = () => {};
+  return (
+    <>
+      <div className="App">
+        <button onClick={onClickFetchData}>Get Data</button>
+      </div>
+    </>
+  );
+}
+
+export default App;
